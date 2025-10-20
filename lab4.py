1994de0eed9f260d1db18f791e003ff10615b485
sumereget <sumerege.tunca@std.yeditepe.edu.tr>
2025-10-20 18:03:14 +0300

    add lab4 throw_rock function

---
+++ lab4.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def throw_rock(m, v0, theta):
+    g = 9.81 #in m/s^2
+    theta = theta * np.pi / 180
+    tf = (2 * v0 * np.sin(theta)) / (g) # in s
+    hm = (v0**2) * np.sin(theta)**2 / (2 * g) #in m
+    R = (v0**2) * np.sin(2 * theta) / (g) #in m
+    vh = v0 * np.cos(theta) # in m/s
+    Kh = 0.5 * m * vh * hm**2 # in J
+    
+    print("For a rock with %5.3f kg mass thrown with %5.3f m/s "\
+          "at an angle of %6.2f degrees:\n" \
+          "Time of flight is %10.1e s\n"
+          "The range in x-direction is %10.1e m\n"\
+          "Maximum height is %10.1e m\n" \
+          "The speed at maximum height is %10.1e m/s\n"\
+          "Kinetic energy at the maximum height is %8.1e J" % (m, v0, theta*180/np.pi, tf, R, hm, vh, Kh))
+    return tf, R, hm, vh, Kh
+myresult = throw_rock(1.5, 0.3, 35.20)
