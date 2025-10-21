d2d143217a9b9de757d5a1aedce6c77bf5872814
sumereget <sumerege.tunca@std.yeditepe.edu.tr>
2025-10-21 17:40:45 +0300

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -15,6 +15,6 @@ def throw_rock(m, v0, theta):
           "The range in x-direction is %10.1e m\n"\
           "Maximum height is %10.1e m\n" \
           "The speed at maximum height is %10.1e m/s\n"\
-          "Kinetic energy at the maximum height is %8.1e J" % (m, v0, theta*180/np.pi, tf, R, hm, vh, Kh))
+          "Kinetic energy at the maximum height is %8.2e J" % (m, v0, theta*180/np.pi, tf, R, hm, vh, Kh))
     return tf, R, hm, vh, Kh
 myresult = throw_rock(1.5, 0.3, 35.20)
